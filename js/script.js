08b54084ead042e7684956ca59022130be893f82
Diego Leal <diego_ldxsbc@hotmail.com>
2021-03-18 16:37:04 -0300

    updates index,css and js

--- js/script.js
+++ js/script.js
@@ -1,56 +1,88 @@
-// Carne - 400 gr por pessoa  + de 6 horas - 650
-// Cerveja - 1200 ml por pessoa + 6 horas - 2000 ml
+// Carne - 400 gr por mulher e 700 gr por homem  + de 6 horas - 700gr por mulher e 1kg por homem
+// Cerveja - 1500 ml por pessoa + 6 horas - 2500 ml
 // Refrigerante/agua - 1000 ml por pessoa + 6 horas 1500 ml
 
 //Crianças valem por 0,5
 //Bebidas = Agua, Refrigerante , Sucos e qualquer coisa não alcoolica
 
-let inputAdultos = document.getElementById("adultos")
-let inputCriancas = document.getElementById("criancas")
-let inputDuracao = document.getElementById("duracao")
+let inputMen = document.getElementById("men")
+let inputWoman = document.getElementById("woman")
+let inputChildren = document.getElementById("children")
+let inputTimer = document.getElementById("timer")
 
-let resultado = document.getElementById("resultado")
+let result = document.getElementById("result")
 
 function calcular() {
     console.log("Calculando...")
 
-    let adultos = inputAdultos.value
-    let criancas = inputCriancas.value
-    let duracao = inputDuracao.value
+    let men = inputMen.value
+    let woman = inputWoman.value
+    let children = inputChildren.value
+    let timer = inputTimer.value
 
-    let qdtTotalCarne = carnePP(duracao) * adultos + (carnePP(duracao) / 2 * criancas)
-    let qdtTotalCerveja = cervejaPP(duracao) * adultos
-    let qdtTotalBebidas = bebidasPP(duracao) * adultos + (bebidasPP(duracao) / 2 * criancas)
+    let valuesMeat = meatFP(timer)
+    let womanMeat = valuesMeat[0] 
+    let menMeat = valuesMeat[1]
+    let childrenMeat = valuesMeat[2]
+    let menTotalMeat = men * menMeat
+    let womanTotalMeat = woman * womanMeat
 
-    resultado.innerHTML = `<p>${qdtTotalCarne / 1000} Kg de Carne</p>`
-    resultado.innerHTML += `<p>${Math.ceil(qdtTotalCerveja/ 355)} Latas de Cerveja</p>`
-    resultado.innerHTML += `<p>${Math.ceil(qdtTotalBebidas/2000)} Garrafas de Bebidas</p>`
+    let valuesBeer = beerFP(timer)
+    let menBeer = valuesBeer[1]
+    let womanBeer = valuesBeer[0]
+    let menTotalBeer = men * menBeer
+    let womanTotalBeer = woman * womanBeer
+
+    let meatTotal = menTotalMeat + womanTotalMeat + childrenMeat
+    let beerTotal = menTotalBeer + womanTotalBeer
+    let drinksTotal = drinksFP(timer) * men + (drinksFP(timer) / 2 * children)
+
+    result.innerHTML = `<p>${meatTotal / 1000} Kg de Carne</p>`
+    result.innerHTML += `<p>${Math.ceil(beerTotal/350)} Latas de Cerveja</p>`
+    result.innerHTML += `<p>${Math.ceil(drinksTotal/2000)} Garrafas de Bebidas</p>`
     
 }
 
-function carnePP(duracao){    
-    if(duracao >= 6){
-        return 650
+function meatFP(timer){    
+
+    if(timer >= 6){
+        womanMeat = 700
+        menMeat = 1000
+        childrenMeat = 500
+        return [womanMeat,menMeat,childrenMeat]
     }
     else{
-        return 400
+        womanMeat = 400
+        menMeat = 700
+        childrenMeat = 300
+        return [womanMeat,menMeat,childrenMeat]
     }
 }
 
-function cervejaPP(duracao){    
-    if(duracao >= 6){
-        return 2000
+function beerFP(timer){    
+    if(timer >= 6){
+        womanBeer = 1800
+        menBeer = 2200
+        return [womanBeer,menBeer]
     }
     else{
-        return 1200
+        womanBeer = 1200
+        menBeer = 1500
+        return [womanBeer,menBeer]
     }
 }
 
-function bebidasPP(duracao){    
-    if(duracao >= 6){
-        return 1500
+function drinksFP(timer){    
+    if(timer >= 6){
+        womanDrinks = 750
+        menDrinks = 1200
+        childrenDrinks = 1500
+        return [womanDrinks,menDrinks,childrenDrinks]
     }
     else{
-        return 1000
+        womanDrinks = 450
+        menDrinks = 600
+        childrenDrinks = 700
+        return [womanDrinks,menDrinks,childrenDrinks]
     }
 }
